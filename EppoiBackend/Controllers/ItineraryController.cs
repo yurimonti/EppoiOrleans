bf93cb2f2261881ad17c69d6040185a5a5285064
yurimonti <48514670+yurimonti@users.noreply.github.com>
2024-02-29 21:07:50 +0100

    TimeToVisit todo fixed

--- EppoiBackend/Controllers/ItineraryController.cs
+++ EppoiBackend/Controllers/ItineraryController.cs
@@ -31,30 +31,22 @@ namespace EppoiBackend.Controllers
             await poiGrain.SetState(long.Parse(id), state.Name, state.Description, state.Address, state.TimeToVisit, state.Coordinate);
             return Ok();
         }
-        //TODO: vedere perche non funziona il for
+        
         [HttpPost]
         public async ValueTask<IActionResult> CreateItinerary([FromBody] ItineraryStateDto state)
         {
             long grainId = _random.NextInt64();
-            double timeToVisit = CalcTime(state.Pois);
+            double timeToVisit = 0d;
+            foreach (var poiId in state.Pois)
+            {
+                var poi = _grainFactory.GetGrain<IPoiGrain>($"poi{poiId}");
+                PoiState aState = await poi.GetPoiState();
+                timeToVisit += aState.TimeToVisit;
+            };
             Console.WriteLine($"final timeToVisit : {timeToVisit}");
             IItineraryGrain itineraryGrain = _grainFactory.GetGrain<IItineraryGrain>($"itinerary{grainId}");
             await itineraryGrain.SetState(grainId, state.Name, state.Description, timeToVisit, state.Pois);
             return Ok();
-        }
-
-        private double CalcTime(List<long> pois)
-        {
-            double timeToVisit = 0;
-            pois.ForEach(async poiId =>
-            {
-                var poiGrain = _grainFactory.GetGrain<IPoiGrain>($"poi{poiId}");
-                var state = await poiGrain.GetPoiState();
-                Console.WriteLine(JsonSerializer.Serialize(state));
-                timeToVisit += state.TimeToVisit;
-                Console.WriteLine($"computed timeToVisit : {timeToVisit}");
-            });
-            return timeToVisit;
         }
     }
 }
